a35e07018527e77bbe9a045a9adb556de1024589
DmitryCost <70369805+DmitryCost@users.noreply.github.com>
2023-02-12 19:27:27 +0700

    pom fix

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/myDb";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root1234";
@@ -12,8 +13,10 @@ public class Util {
     public static Connection getConnection() {
         Connection connection = null;
         try {
+            Class.forName(DRIVER);
             connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        } catch (SQLException e) {
+            connection.setAutoCommit(false);
+        } catch (SQLException | ClassNotFoundException e) {
             System.out.println("Not connection" + e);
         }
         return connection;
