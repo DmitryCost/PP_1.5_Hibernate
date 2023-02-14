01c87e10a0d689f2ba1288adca5b3d9f3351e6bd
DmitryCost <70369805+DmitryCost@users.noreply.github.com>
2023-02-14 23:43:12 +0700

    pom fix

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,95 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.*;
 
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+    private final static SessionFactory sF = Util.getSessionFactory();
     public UserDaoHibernateImpl() {
-
     }
-
 
     @Override
     public void createUsersTable() {
-
+        try (Session session = sF.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.createSQLQuery("CREATE TABLE IF NOT EXISTS myDb.users" +
+                    " (id mediumint not null auto_increment, name VARCHAR(45), " +
+                    "lastname VARCHAR(45), " +
+                    "age INT, " + "PRIMARY KEY (id))").executeUpdate();
+            transaction.commit();
+            System.out.println("Created new table");
+        } catch (HibernateException e) {
+            System.out.println("Empty table" + e);
+        }
     }
 
     @Override
     public void dropUsersTable() {
-
+        try (Session session = sF.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.createSQLQuery("DROP TABLE IF EXISTS myDb.users").executeUpdate();
+            transaction.commit();
+            System.out.println("Users table drop");
+        } catch (HibernateException e) {
+            System.out.println("Table doesn't exist" + e);
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        Transaction transaction = null;
+        try (Session session = sF.openSession()) {
+            transaction = session.beginTransaction();
+            session.save(new User(name, lastName, age));
+            transaction.commit();
+        } catch (HibernateException e) {
+            System.out.println("User doesn't save" + e);
+            transaction.rollback();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        Transaction transaction = null;
+        try (Session session = sF.openSession()) {
+            transaction = session.beginTransaction();
+            session.delete(session.get(User.class, id));
+            transaction.commit();
+            System.out.println("User delete");
+        } catch (HibernateException e) {
+            System.out.println("Don't delete users by id" + e);
+            transaction.rollback();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
+        Transaction transaction = null;
+        try (Session session = sF.openSession()) {
+            transaction = session.beginTransaction();
+            transaction.commit();
+            return session.createQuery("from User", User.class).list();
+        } catch (HibernateException e) {
+            System.out.println("Get all users is failed" + e);
+            transaction.rollback();
+        }
         return null;
     }
 
     @Override
     public void cleanUsersTable() {
-
+        Transaction transaction = null;
+        try (Session session = sF.openSession()) {
+            transaction = session.beginTransaction();
+            session.createQuery("delete User").executeUpdate();
+            transaction.commit();
+            System.out.println("Table is cleaned");
+        } catch (HibernateException e) {
+            System.out.println("Table doesn't clean" + e);
+            transaction.rollback();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,113 +1,35 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.util.Util;
-
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class UserDaoJDBCImpl implements UserDao {
-    private final Connection connection = Util.getConnection();
+
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
-        String sql = "CREATE TABLE IF NOT EXISTS myDb" +
-                "(id INT NOT NULL AUTO_INCREMENT," +
-                "name VARCHAR(45)," +
-                "lastName VARCHAR(45)," +
-                "age INT," + "PRIMARY KEY (id))";
-        try (Statement statement = connection.createStatement()) {
-            statement.executeUpdate(sql);
-            System.out.println("Created new table");
-            connection.commit();
-        } catch (SQLException e) {
-            System.out.println("Empty table" + e);
-        }
+
     }
 
     public void dropUsersTable() {
-        String sql = "DROP TABLE IF EXISTS myDb";
-        try (Statement statement = connection.createStatement()) {
-            statement.executeUpdate(sql);
-            System.out.println("Users table drop");
-            connection.commit();
-        } catch (SQLException e) {
-            System.out.println("Table doesn't exist" + e);
-        }
+
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        String sql = "INSERT INTO myDb(name, lastName, age) VALUES (?, ?, ?)";
-        try (PreparedStatement pS = connection.prepareStatement(sql)) {
-            pS.setString(1, name);
-            pS.setString(2, lastName);
-            pS.setByte(3, age);
-            pS.executeUpdate();
-            connection.commit();
-        } catch (SQLException e) {
-            System.out.println("User doesn't save" + e);
-            try {
-                connection.rollback();
-            } catch (SQLException ex) {
-                throw new RuntimeException(ex);
-            }
-        }
+
     }
 
     public void removeUserById(long id) {
-        String sql = "DELETE FORM myDb WHERE id = ?";
-        try (PreparedStatement pS = connection.prepareStatement(sql)) {
-            pS.setLong(1, id);
-            System.out.println("User delete");
-            connection.commit();
-        } catch (SQLException e) {
-            System.out.println("Don't delete users by id" + e);
-            try {
-                connection.rollback();
-            } catch (SQLException ex) {
-                throw new RuntimeException(ex);
-            }
-        }
+
     }
 
     public List<User> getAllUsers() {
-        String sql = "SELECT * FROM myDb";
-        List<User> listUsers = new ArrayList<>();
-        try (ResultSet resultSet = connection.createStatement().executeQuery(sql)) {
-            connection.commit();
-            while(resultSet.next()) {
-                User user = new User(resultSet.getString("name"),
-                        resultSet.getString("lastName"), resultSet.getByte("age"));
-                user.setId(resultSet.getLong("id"));
-                listUsers.add(user);
-            }
-        } catch (SQLException e) {
-            System.out.println("Get all users is failed" + e);
-            try {
-                connection.rollback();
-            } catch (SQLException ex) {
-                throw new RuntimeException(ex);
-            }
-        }
-        return listUsers;
+        return null;
     }
 
     public void cleanUsersTable() {
-        String sql = "TRUNCATE myDb";
-        try (Statement statement = connection.createStatement()) {
-            statement.executeUpdate(sql);
-            System.out.println("Table is cleaned");
-            connection.commit();
-        } catch (SQLException e) {
-            System.out.println("Table doesn't clean" + e);
-            try {
-                connection.rollback();
-            } catch (SQLException ex) {
-                throw new RuntimeException(ex);
-            }
-        }
+
     }
 }
